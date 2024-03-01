e37fc7228fe6bd9c39922ad39be9f815abcb8392
SteellerzCod4 <83817014+SteellerzCod4@users.noreply.github.com>
2024-03-01 21:34:40 +0300

    add func do_cool_stuff (#2)
    * add func do_cool_stuff
    
    * add func super_func

---
+++ first/new_feature.py
@@ -0,0 +1,11 @@
+def do_cool_stuff(number):
+    return str(number) * 10
+
+
+def super_func(number):
+    return number / 1000
+
+
+if __name__ == "__main__":
+    print(do_cool_stuff(100))
+    print(super_func(57))
